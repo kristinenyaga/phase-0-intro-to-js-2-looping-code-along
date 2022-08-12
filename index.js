0a75c2062fe094caa64e20471691c6a2701b4d2e
krisine <kristine.nyaga@student.moringaschool.com>
2022-08-12 09:41:49 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+  
+  }
+  const gifts = ["teddy bear", "drone", "doll"];
+  function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+      debugger;
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(arrayName,birthMessage){
+    let messages=[]
+    for(let i=0;i<arrayName.length;i++){
+        
+        messages.push(`Thank you, ${arrayName[i]}, for the wonderful ${birthMessage} gift!`)
+        
+       
+    }
+    console.log(messages)
+     return messages
+   
+       
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+function countDown(){
+  let count=10;
+  while(count>-1){
+    console.log(count--);
+
+  }
+}
+countDown()
